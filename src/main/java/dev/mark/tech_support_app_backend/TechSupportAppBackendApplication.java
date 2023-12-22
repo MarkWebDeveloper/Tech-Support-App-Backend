ceb7f7e0e204b03fd4c4bab78b264ff6d6cb8848
Mark Kil <markkilcanarias@gmail.com>
2023-12-22 14:30:17 +0100

    first commit

---
+++ src/main/java/dev/mark/tech_support_app_backend/TechSupportAppBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.mark.tech_support_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSupportAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSupportAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mark/tech_support_app_backend/TechSupportAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mark.tech_support_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSupportAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
